a13c866838723b13410f8079f7d5b33dec48fd46
Aery1e <132122059+Aery1e@users.noreply.github.com>
2024-02-26 04:33:20 +0000

    Part 1, MISSING_ARGUMENT + DUPLICATE_ARGUMENT basics implemented for -iocpr, UNRECOGNIZED_ARGUMENT implemented.

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -14,35 +14,60 @@ int main(int argc, char **argv) {
     extern char *optarg; extern int optind;
     int c, err = 0; 
     char *iname, *oname;
-    int iflag, oflag, cflag, pflag, rflag; 
+    int iflag = 0; 
+    int oflag = 0;
+    int cflag = 0; 
+    int pflag = 0;
+    int rflag = 0; 
     while ((c = getopt(argc, argv, "i:o:")) != -1) {
 		switch (c) {
 		case 'i':
-            if(optarg == ""){
-                printf("missing -i argument");
-                return MISSING_ARGUMENT;
+            if(optarg[0] == '-') return MISSING_ARGUMENT;
+            if(iflag == 1){
+                return DUPLICATE_ARGUMENT;
             }
             iflag = 1;
             iname = optarg;
+            printf("iname = %s\n", iname);
 			break;
 		case 'o':
-            if(optarg == ""){
-                printf("missing -o argument");
-                return MISSING_ARGUMENT;
+            if(optarg[0] == '-') return MISSING_ARGUMENT;
+            if(oflag == 1){
+                return DUPLICATE_ARGUMENT;
             }
             oflag = 1;
             oname = optarg;
+            printf("oname = %s\n", oname);
 			break;
 		case 'c':
+            if(optarg[0] == '-') return MISSING_ARGUMENT;
 			break;
+            if(cflag == 1){
+                return DUPLICATE_ARGUMENT;
+            }
+            cflag = 1;
+            break;
 		case 'p':
+            if(optarg[0] == '-') return MISSING_ARGUMENT;
+            if(pflag == 1){
+                return DUPLICATE_ARGUMENT;
+            }
+            pflag = 1;
 			break;
 		case 'r':
+            if(optarg[0] == '-') return MISSING_ARGUMENT;
+            if(rflag == 1){
+                return DUPLICATE_ARGUMENT;
+            }
+            rflag = 1;
 			break;
 		case '?':
 			err = 1;
 			break;
 		}
+        if(err == 1){
+            return UNRECOGNIZED_ARGUMENT;
+        }
     }
     (void)argc;
     (void)argv;
