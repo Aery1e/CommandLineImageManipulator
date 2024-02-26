dad8ed68d4ce234695c61012bcbb428ddefb5fb5
Aery1e <132122059+Aery1e@users.noreply.github.com>
2024-02-26 04:04:26 +0000

    Part 1, verifying build functionality

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -10,6 +10,40 @@
 #include <unistd.h> 
 
 int main(int argc, char **argv) {
+    //Part 1 : Return Exceptions
+    extern char *optarg; extern int optind;
+    int c, err = 0; 
+    char *iname, *oname;
+    int iflag, oflag, cflag, pflag, rflag; 
+    while ((c = getopt(argc, argv, "i:o:")) != -1) {
+		switch (c) {
+		case 'i':
+            if(optarg == ""){
+                printf("missing -i argument");
+                return MISSING_ARGUMENT;
+            }
+            iflag = 1;
+            iname = optarg;
+			break;
+		case 'o':
+            if(optarg == ""){
+                printf("missing -o argument");
+                return MISSING_ARGUMENT;
+            }
+            oflag = 1;
+            oname = optarg;
+			break;
+		case 'c':
+			break;
+		case 'p':
+			break;
+		case 'r':
+			break;
+		case '?':
+			err = 1;
+			break;
+		}
+    }
     (void)argc;
     (void)argv;
 
